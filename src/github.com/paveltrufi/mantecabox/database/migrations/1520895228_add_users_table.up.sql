40b8557906d1adedefe761f89bc88b4e3f822747
Pavel Razgovorov <pavel.granalacant@gmail.com>
2018-03-18 00:09:29 +0100

    Arreglada migración para no permitir usuarios y contraseñas más cortas de lo esperado

--- src/github.com/paveltrufi/mantecabox/database/migrations/1520895228_add_users_table.up.sql
+++ src/github.com/paveltrufi/mantecabox/database/migrations/1520895228_add_users_table.up.sql
@@ -1,4 +1,4 @@
--- Como esta va a ser la primera tabla con timestamps, nos creamos aquí la función que se usará en los triggers
+/* Como esta va a ser la primera tabla con timestamps, nos creamos aquí la función que se usará en los triggers */
 CREATE OR REPLACE FUNCTION trigger_set_timestamp()
   RETURNS TRIGGER AS $$
 BEGIN
@@ -12,8 +12,11 @@ CREATE TABLE IF NOT EXISTS users (
   created_at TIMESTAMP   NOT NULL DEFAULT NOW(),
   updated_at TIMESTAMP   NOT NULL DEFAULT NOW(),
   deleted_at TIMESTAMP,
-  username   VARCHAR(20) CONSTRAINT users_pk PRIMARY KEY,
+  username   VARCHAR(20)
+    CONSTRAINT users_pk PRIMARY KEY
+    CONSTRAINT username_not_empty CHECK (length(username) > 0),
   password   VARCHAR(60) NOT NULL
+    CONSTRAINT password_not_empty CHECK (length(password) > 0)
 );
 
 CREATE TRIGGER set_timestamp
