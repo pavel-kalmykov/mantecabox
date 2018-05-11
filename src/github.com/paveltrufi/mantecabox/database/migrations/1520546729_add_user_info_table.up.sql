7371c6e7dc85e91a867984cd5eaec6f6623a160d
Raúl Pera Pairó <raul_pairo@icloud.com>
2018-05-11 17:48:38 +0200

    Borrado de integraciones innecesarias

--- src/github.com/paveltrufi/mantecabox/database/migrations/1520546729_add_user_info_table.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE userinfo
-(
-  uid        SERIAL                 NOT NULL,
-  username   CHARACTER VARYING(100) NOT NULL,
-  departname CHARACTER VARYING(500) NOT NULL,
-  Created    DATE,
-  CONSTRAINT userinfo_pkey PRIMARY KEY (uid)
-) WITH (OIDS = FALSE
-);
