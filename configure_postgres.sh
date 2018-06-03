65d4cdf8747423897e3a7a0e033d341dd1c817e2
Pavel Razgovorov <paveltrufi@hotmail.com>
2018-06-03 14:40:44 +0200

    Añadido script de creación de base de datos de prueba para Docker
    Signed-off-by: Pavel Razgovorov <paveltrufi@hotmail.com>

--- configure_postgres.sh
+++ configure_postgres.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Preparando copia configuraciones y certificados"
+echo "Preparing certificates"
 ls /srv/
 cp /srv/postgresql.conf /var/lib/postgresql/data/postgresql.conf
 cp /srv/server.crt /var/lib/postgresql/data/server.crt
@@ -8,4 +8,4 @@ cp /srv/server.key /var/lib/postgresql/data/server.key
 chmod -R 600 /var/lib/postgresql/data/server.crt
 chmod -R 600 /var/lib/postgresql/data/server.key
 chown -R postgres: /var/lib/postgresql/data/
-echo "Copiados todos los ficheros"
+echo "Everything copied; SSL ready"
