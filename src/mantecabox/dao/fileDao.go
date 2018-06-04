3a77713bb755389db7165f1e5cfddb587b936642
Pavel Razgovorov <paveltrufi@hotmail.com>
2018-06-04 12:49:25 +0200

    Mezclados archivos DAO para tener la interfaz y la implementación de cada cosa en un mismo fichero
    Signed-off-by: Pavel Razgovorov <paveltrufi@hotmail.com>

--- src/mantecabox/dao/fileDao.go
+++ src/mantecabox/dao/fileDao.go
@@ -1,11 +1,172 @@
 package dao
 
-import "mantecabox/models"
+import (
+	"database/sql"
+	"errors"
+	"fmt"
 
-type FileDao interface {
-	GetAll(user *models.User) ([]models.File, error)
-	GetByPk(filename string, user *models.User) (models.File, error)
-	Create(f *models.File) (models.File, error)
-	Update(id int64, f *models.File) (models.File, error)
-	Delete(filename string, user *models.User) error
+	"mantecabox/models"
+)
+
+const (
+	getAllFilesQuery = `SELECT
+  f.*,
+  u.*
+FROM files f
+  JOIN users u ON f.owner = u.email
+WHERE f.deleted_at IS NULL AND u.deleted_at IS NULL AND u.email = $1`
+	getFileByNameAndOwner = `SELECT
+  f.*,
+  u.*
+FROM files f
+  JOIN users u ON f.owner = u.email
+WHERE f.deleted_at IS NULL AND u.deleted_at IS NULL AND f.name = $1 AND u.email = $2`
+	insertFileQuery = `INSERT INTO files (name, owner) VALUES ($1, $2) RETURNING *;`
+	updateFileQuery = `UPDATE files SET name = $1, owner = $2 WHERE id = $3 RETURNING *`
+	deleteFileQuery = "UPDATE files SET deleted_at = NOW() WHERE name = $1 AND owner = $2"
+)
+
+type (
+	FileDao interface {
+		GetAll(user *models.User) ([]models.File, error)
+		GetByPk(filename string, user *models.User) (models.File, error)
+		Create(f *models.File) (models.File, error)
+		Update(id int64, f *models.File) (models.File, error)
+		Delete(filename string, user *models.User) error
+	}
+
+	FilePgDao struct {
+	}
+)
+
+func (dao FilePgDao) GetAll(user *models.User) ([]models.File, error) {
+	res, err := withDb(func(db *sql.DB) (interface{}, error) {
+		files := make([]models.File, 0)
+		rows, err := db.Query(getAllFilesQuery, user.Email)
+		if err != nil {
+			daoLog.Info("Unable to execute FilePgDao.GetAll() query. Reason:", err)
+			return nil, err
+		}
+
+		for rows.Next() {
+			var file models.File
+			err := scanFileRowWithUser(rows, &file)
+			if err != nil {
+				daoLog.Info("Unable to execute FilePgDao.GetAll() query. Reason:", err)
+				return nil, err
+			}
+			files = append(files, file)
+		}
+
+		daoLog.Debug("Queried", len(files), "files")
+		return files, err
+	})
+	return res.([]models.File), err
+}
+
+func (dao FilePgDao) GetByPk(filename string, user *models.User) (models.File, error) {
+	res, err := withDb(func(db *sql.DB) (interface{}, error) {
+		file := models.File{}
+		row := db.QueryRow(getFileByNameAndOwner, filename, user.Email)
+		err := scanFileRowWithUser(row, &file)
+		if err != nil {
+			daoLog.Debug("Unable to execute FilePgDao.GetByPk(email string) query. Reason:", err)
+		} else {
+			daoLog.Debug("Retrieved file ", file)
+		}
+		return file, err
+	})
+	return res.(models.File), err
+}
+
+func (dao FilePgDao) Create(file *models.File) (models.File, error) {
+	res, err := withDb(func(db *sql.DB) (interface{}, error) {
+		var createdFile models.File
+		row := db.QueryRow(insertFileQuery, file.Name, file.Owner.Email)
+		err := scanFileRow(row, &createdFile)
+		if err != nil {
+			daoLog.Info("Unable to execute FilePgDao.Create(file models.File) query. Reason:", err)
+		} else {
+			daoLog.Debug("Created file: ", createdFile)
+		}
+		owner, err := UserPgDao{}.GetByPk(createdFile.Owner.Email)
+		createdFile.Owner = owner
+		return createdFile, err
+	})
+	return res.(models.File), err
+}
+
+func (dao FilePgDao) Update(id int64, file *models.File) (models.File, error) {
+	res, err := withDb(func(db *sql.DB) (interface{}, error) {
+		var updatedFile models.File
+		row := db.QueryRow(updateFileQuery,
+			file.Name, file.Owner.Email, id,
+		)
+		err := scanFileRow(row, &updatedFile)
+		if err != nil {
+			daoLog.Info("Unable to execute FilePgDao.Update(id int64, file models.File) query. Reason:", err)
+		} else {
+			daoLog.Debug("Created file", updatedFile)
+		}
+		owner, err := UserPgDao{}.GetByPk(updatedFile.Owner.Email)
+		updatedFile.Owner = owner
+		return updatedFile, err
+	})
+	return res.(models.File), err
+}
+
+func (dao FilePgDao) Delete(filename string, user *models.User) error {
+	_, err := withDb(func(db *sql.DB) (interface{}, error) {
+		result, err := db.Exec(deleteFileQuery, filename, user.Email)
+		if err != nil {
+			daoLog.Info("Unable to execute FilePgDao.Delete(id int64) query. Reason:", err)
+		} else {
+			var rowsAffected int64
+			rowsAffected, err = result.RowsAffected()
+			if err != nil {
+				daoLog.Info("Some error occured during deleting:", err)
+			} else {
+				switch {
+				case rowsAffected == 0:
+					err = errors.New("not found")
+				case rowsAffected > 1:
+					err = errors.New("more than one deleted")
+				}
+				if err != nil {
+					daoLog.Debug(fmt.Sprintf(`Unable to delete %v's' file "%v". Reason %v`, user.Email, filename, err))
+				} else {
+					daoLog.Debug(fmt.Sprintf(`%v's' file "%v" successfully deleted.`, user.Email, filename))
+				}
+			}
+		}
+		return nil, err
+	})
+	return err
+}
+
+func scanFileRow(scanner polimorphicScanner, file *models.File) error {
+	err := scanner.Scan(&file.Id,
+		&file.CreatedAt,
+		&file.UpdatedAt,
+		&file.DeletedAt,
+		&file.Name,
+		&file.Owner.Email)
+	return err
+}
+
+func scanFileRowWithUser(scanner polimorphicScanner, file *models.File) error {
+	err := scanner.Scan(&file.Id,
+		&file.CreatedAt,
+		&file.UpdatedAt,
+		&file.DeletedAt,
+		&file.Name,
+		&file.Owner.Email,
+		&file.Owner.CreatedAt,
+		&file.Owner.UpdatedAt,
+		&file.Owner.DeletedAt,
+		&file.Owner.Email,
+		&file.Owner.Password,
+		&file.Owner.TwoFactorAuth,
+		&file.Owner.TwoFactorTime)
+	return err
 }
--- src/mantecabox/dao/loginAttemptDao.go
+++ src/mantecabox/dao/loginAttemptDao.go
@@ -1,12 +1,146 @@
 package dao
 
 import (
+	"database/sql"
+
 	"mantecabox/models"
+	"mantecabox/utilities"
 )
 
-type LoginAttempDao interface {
-	GetByUser(email string) ([]models.LoginAttempt, error)
-	GetLastNByUser(email string, n int) ([]models.LoginAttempt, error)
-	GetSimilarAttempts(attempt *models.LoginAttempt) ([]models.LoginAttempt, error)
-	Create(attempt *models.LoginAttempt) (models.LoginAttempt, error)
+const (
+	getLoginAttemptsByUserQuery = `SELECT
+  la.*,
+  u.*
+FROM login_attempts la
+  JOIN users u ON la."user" = u.email
+WHERE u.deleted_at IS NULL AND la."user" = $1`
+	getLastNLoginAttemptsByUserQuery = `SELECT *
+FROM (SELECT
+        la.*,
+        u.*
+      FROM login_attempts la
+        JOIN users u ON la."user" = u.email
+      WHERE u.deleted_at IS NULL AND la."user" = $1
+      ORDER BY la.id DESC
+      LIMIT $2) as reversed
+ORDER BY reversed.id`
+	insertLoginAttemptQuery = `INSERT INTO login_attempts ("user", user_agent, ip, successful) VALUES ($1, $2, $3, $4) RETURNING *;`
+)
+
+type (
+	LoginAttemptPgDao struct {
+	}
+
+	LoginAttempDao interface {
+		GetByUser(email string) ([]models.LoginAttempt, error)
+		GetLastNByUser(email string, n int) ([]models.LoginAttempt, error)
+		GetSimilarAttempts(attempt *models.LoginAttempt) ([]models.LoginAttempt, error)
+		Create(attempt *models.LoginAttempt) (models.LoginAttempt, error)
+	}
+)
+
+func withDb(f func(db *sql.DB) (interface{}, error)) (interface{}, error) {
+	db, err := utilities.GetPgDb()
+	if err != nil {
+		daoLog.Fatal("Unable to connnect with database: " + err.Error())
+		return models.LoginAttempt{}, err
+	}
+	defer db.Close()
+	return f(db)
+}
+
+func scanLoginAttemptWithNestedUser(rows *sql.Rows) ([]models.LoginAttempt, error) {
+	attempts := make([]models.LoginAttempt, 0)
+	for rows.Next() {
+		var attempt models.LoginAttempt
+		var user string
+		err := rows.Scan(&attempt.Id,
+			&attempt.CreatedAt,
+			&user,
+			&attempt.UserAgent,
+			&attempt.IP,
+			&attempt.Successful,
+			&attempt.User.CreatedAt,
+			&attempt.User.UpdatedAt,
+			&attempt.User.DeletedAt,
+			&attempt.User.Email,
+			&attempt.User.Password,
+			&attempt.User.TwoFactorAuth,
+			&attempt.User.TwoFactorTime,
+		)
+		if err != nil {
+			daoLog.Info("Unable to execute LoginAttemptPgDao.scanLoginAttemptWithNestedUser() scan. Reason:", err)
+			return nil, err
+		}
+		attempts = append(attempts, attempt)
+	}
+
+	daoLog.Debug("Queried", len(attempts), "login attempts")
+	return attempts, nil
+}
+
+func (dao LoginAttemptPgDao) GetByUser(email string) ([]models.LoginAttempt, error) {
+	res, err := withDb(func(db *sql.DB) (interface{}, error) {
+		rows, err := db.Query(getLoginAttemptsByUserQuery, email)
+		if err != nil {
+			daoLog.Info("Unable to execute LoginAttemptPgDao.GetByUser() query. Reason:", err)
+			return nil, err
+		}
+		return scanLoginAttemptWithNestedUser(rows)
+	})
+	return res.([]models.LoginAttempt), err
+}
+
+func (dao LoginAttemptPgDao) GetLastNByUser(email string, n int) ([]models.LoginAttempt, error) {
+	if n < 0 {
+		return dao.GetByUser(email)
+	}
+	res, err := withDb(func(db *sql.DB) (interface{}, error) {
+		rows, err := db.Query(getLastNLoginAttemptsByUserQuery, email, n)
+		if err != nil {
+			daoLog.Info("Unable to execute LoginAttemptPgDao.GetLastNByUser() query. Reason:", err)
+			return nil, err
+		}
+		return scanLoginAttemptWithNestedUser(rows)
+	})
+	return res.([]models.LoginAttempt), err
+}
+
+func (dao LoginAttemptPgDao) Create(attempt *models.LoginAttempt) (models.LoginAttempt, error) {
+	res, err := withDb(func(db *sql.DB) (interface{}, error) {
+		var createdAttempt models.LoginAttempt
+		err := db.QueryRow(insertLoginAttemptQuery, attempt.User.Email, attempt.UserAgent, attempt.IP, attempt.Successful).
+			Scan(&createdAttempt.Id, &createdAttempt.CreatedAt, &createdAttempt.User.Email,
+				&createdAttempt.UserAgent, &createdAttempt.IP, &createdAttempt.Successful)
+		if err != nil {
+			daoLog.Info("Unable to execute FilePgDao.Create(file models.File) query. Reason:", err)
+			return createdAttempt, err
+		} else {
+			daoLog.Debug("Created file: ", createdAttempt)
+		}
+		owner, err := UserPgDao{}.GetByPk(createdAttempt.User.Email)
+		createdAttempt.User = owner
+		return createdAttempt, err
+	})
+	return res.(models.LoginAttempt), err
+}
+
+func (dao LoginAttemptPgDao) GetSimilarAttempts(attempt *models.LoginAttempt) ([]models.LoginAttempt, error) {
+	res, err := withDb(func(db *sql.DB) (interface{}, error) {
+		rows, err := db.Query(`SELECT
+		la.*,
+		u.*
+		FROM login_attempts la
+		JOIN users u ON la."user" = u.email
+		WHERE u.deleted_at IS NULL
+		AND la."user" = $1
+		AND la.user_agent = $2
+		AND la.ip = $3;`, attempt.User.Email, attempt.UserAgent, attempt.IP)
+		if err != nil {
+			daoLog.Info("Unable to execute LoginAttemptPgDao.GetSimilarAttempts() query. Reason:", err)
+			return nil, err
+		}
+		return scanLoginAttemptWithNestedUser(rows)
+	})
+	return res.([]models.LoginAttempt), err
 }
