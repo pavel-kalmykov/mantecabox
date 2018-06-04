14349538a55b542260386bd2b853154744227b53
Pavel Razgovorov <paveltrufi@hotmail.com>
2018-06-04 23:44:02 +0200

    Añadida operación de subida si no existe remotamente
    Signed-off-by: Pavel Razgovorov <paveltrufi@hotmail.com>

--- src/mantecabox/cli/watcher.go
+++ src/mantecabox/cli/watcher.go
@@ -14,6 +14,8 @@ import (
 	"gopkg.in/guregu/null.v3"
 )
 
+var mantecaboxDir = ""
+
 func monitorChanges(w *watcher.Watcher) {
 	for {
 		select {
@@ -24,41 +26,67 @@ func monitorChanges(w *watcher.Watcher) {
 				fmt.Fprintln(os.Stderr, err)
 			}
 
-			localFiles := make([]models.FileDTO, len(fileInfos))
+			localFiles := make([]models.FileDTO, 0)
 			for _, fileInfo := range fileInfos {
-				localFiles = append(localFiles, models.FileDTO{
-					TimeStamp:      models.TimeStamp{UpdatedAt: null.Time{Time: fileInfo.ModTime(), Valid: true}},
-					Name:           fileInfo.Name(),
-					PermissionsStr: permbits.FileMode(fileInfo.Mode()).String(),
-				})
+				if fileInfo.Name() != "" {
+					localFiles = append(localFiles, models.FileDTO{
+						TimeStamp:      models.TimeStamp{UpdatedAt: null.Time{Time: fileInfo.ModTime(), Valid: true}},
+						Name:           fileInfo.Name(),
+						PermissionsStr: permbits.FileMode(fileInfo.Mode()).String(),
+					})
+				}
 			}
 
 			token, err := GetToken()
 			if err != nil {
-				fmt.Fprintln(os.Stderr, err)
+				fmt.Fprintln(os.Stderr, "unable to get token:", err)
 				return
 			}
 
 			ids, names, dates, permissions, err := getFilesList(token)
-			if err != nil {
-				fmt.Fprintln(os.Stderr, err)
+			if err != nil && err.Error() != "there are no files in our servers. Upload one" {
+				fmt.Fprintln(os.Stderr, "unable to retrieve files:", err)
 				return
 			}
-			remoteFiles := make([]models.FileDTO, len(ids))
+			remoteFiles := make([]models.FileDTO, 0)
 			for i := 0; i < len(ids); i++ {
 				remoteFiles = append(remoteFiles, models.FileDTO{
 					TimeStamp:      models.TimeStamp{UpdatedAt: null.Time{Time: dates[i].Time(), Valid: true}},
-					Name:           names[i].Raw,
-					PermissionsStr: permissions[i].Raw,
+					Name:           names[i].String(),
+					PermissionsStr: permissions[i].String(),
 				})
 			}
 
+			compareFileLists(localFiles, remoteFiles, token)
 		case err := <-w.Error:
 			fmt.Fprintln(os.Stderr, err)
 		case <-w.Closed:
 			return
 		}
 	}
+}
+
+func compareFileLists(localFiles []models.FileDTO, remoteFiles []models.FileDTO, token string) {
+	for _, localFile := range localFiles {
+		if !fileInSlice(localFile, remoteFiles) {
+			filePath := mantecaboxDir + localFile.Name
+			_, err := uploadFile(filePath, token)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "unable to upload file:", err)
+				return
+			}
+			fmt.Println("File", localFile.Name, "uploaded")
+		}
+	}
+}
+
+func fileInSlice(a models.FileDTO, list []models.FileDTO) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
 }
 
 func StartDaemon() {
@@ -70,7 +98,8 @@ func StartDaemon() {
 		fmt.Fprintln(os.Stderr, err)
 	}
 	dir += "/Mantecabox/"
-	if err := w.Add(dir); err != nil {
+	mantecaboxDir = dir
+	if err := w.Add(mantecaboxDir); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 	}
 
